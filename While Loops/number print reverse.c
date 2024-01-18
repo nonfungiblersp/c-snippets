2751655ec6a27843ceda1425b760ef51618d9326
saiprasad <130814487+nonfungiblersp@users.noreply.github.com>
2024-01-18 16:42:33 +0530

    reverse the number in order of digits
    it will make the number in the reverse order.

---
+++ While Loops/number print reverse.c
@@ -0,0 +1,24 @@
+/*
+Write a C program to enter a number and print its reverse.
+*/
+
+int main (void)
+{
+	int i,x,r,sum=0,temp;
+	
+	printf("Enter number :");
+	scanf("%d",&x);
+	
+	temp = x;
+	
+	i=0;
+	while(i<=x)
+	{
+		r = x%10;
+		sum = sum*10 + r;
+		x/=10;
+		i++;
+	}
+	printf("The Original number is = %d\n",temp);
+	printf("The reverse of  number is = %d\n",sum);
+}
